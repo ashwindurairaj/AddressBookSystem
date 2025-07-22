a33b116db471dec8b28a23a8ae8aae411afdbe4e
ashwindurairaj <ashwindurairaj22@gmail.com>
2025-07-22 17:30:06 +0530

    [ashwin dev commit]

--- src/modal/AddressBook.ts
+++ src/modal/AddressBook.ts
@@ -1,1 +1,21 @@
-console.log("Addressbook class")
+
+import {ContactPerson} from "./ContactPeron"
+export class AddressBook{
+
+    private contacts:ContactPerson[]=[]
+
+    addAccount(contact:ContactPerson):void
+    {
+        
+        this.contacts.push(contact)
+       
+        
+    }
+    
+    getAllContacts(): void {
+    console.log("📇 Contact List:");
+    this.contacts.forEach((contact, i) => console.log(`${i + 1}. ${contact.toString()}`));
+  }
+    
+
+}
