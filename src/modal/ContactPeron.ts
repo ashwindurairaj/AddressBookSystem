131a19024d768134ff05bb37b97da70c94bb69a6
ashwindurairaj <ashwindurairaj22@gmail.com>
2025-07-23 15:19:20 +0530

    [Ashwin] edit the existing contact person using name

--- src/modal/ContactPeron.ts
+++ src/modal/ContactPeron.ts
@@ -1,1 +1,21 @@
-console.log("Contact")
+export class ContactPerson{
+
+   constructor(
+    public firstName:string,
+    public lastName:string,
+    public address:string,
+    public city:string,
+    public state:string,
+    public zipcode:number,
+    public phoneNumber:number,
+    public email:string,
+   ){}
+
+   toString():string{
+
+          return `${this.firstName} ${this.lastName}, ${this.city}, ${this.state}, ${this.zipcode}, ${this.phoneNumber}, ${this.email}`;
+   }
+
+   
+
+}
